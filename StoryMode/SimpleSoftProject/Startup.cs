e0ae64305ced6bcded4a1c8b55fa459e68758758
Anton Nikolov <antonio85pd@gmail.com>
2016-09-06 22:31:26 +0300

    reading data, process, output

---
+++ StoryMode/SimpleSoftProject/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SimpleSoftProject.IO;
+using SimpleSoftProject.Repository;
+
+namespace SimpleSoftProject
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            StudentsRepository.InitializeData();
+            StudentsRepository.GetAllStudentsFromCourse("Unity");
+        }
+    }
+}
