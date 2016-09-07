b89b1f229d857caa3f5fd52d1dd3e67ec598044d
Anton Nikolov <antonio85pd@gmail.com>
2016-09-07 18:15:54 +0300

    Compare files, exception handling

--- StoryMode/SimpleSoftProject/Startup.cs
+++ StoryMode/SimpleSoftProject/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SimpleSoftProject.IO;
+using SimpleSoftProject.Judge;
 using SimpleSoftProject.Repository;
 
 namespace SimpleSoftProject
@@ -8,8 +9,10 @@ namespace SimpleSoftProject
     {
         static void Main(string[] args)
         {
-            StudentsRepository.InitializeData();
-            StudentsRepository.GetAllStudentsFromCourse("Unity");
+            /*StudentsRepository.InitializeData();
+            StudentsRepository.GetAllStudentsFromCourse("Unity");*/
+            //Tester.CompareContent(@"E:\Computer Staff\SoftUni\Labs\AdvancedLab\test2.txt", @"E:\Computer Staff\SoftUni\Labs\AdvancedLab\test3.txt");
+            //IOManager.TraverseDirectory(0);
         }
     }
 }
---
+++ StoryMode/SimpleSoftProject/StaticData/SessionData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleSoftProject.StaticData
+{
+    public static class SessionData
+    {
+        public static string currentPath = Directory.GetCurrentDirectory();
+    }
+}
